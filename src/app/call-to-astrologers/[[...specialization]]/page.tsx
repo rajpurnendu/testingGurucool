ad96e7e02179b227e11b9662db33583e8c064bb9
Purnendu <mpurnendu337@gmail.com>
2024-01-18 14:23:40 +0530

    Updated

--- src/app/call-to-astrologers/[[...specialization]]/page.tsx
+++ src/app/call-to-astrologers/[[...specialization]]/page.tsx
@@ -2,6 +2,7 @@ import ConsultCard from "@/components/consult_page/ConsultCard";
 import Searchbar from "@/components/consult_page/Searchbar";
 import Speciality from "@/components/consult_page/Speciality";
 import { GET_Spec_Astrologer } from "../../../lib/data";
+import Link from "next/link";
 
 export default async function Page({
   params,
